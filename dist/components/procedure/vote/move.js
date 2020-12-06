c1fa86d84c3f58396e959046b02b8463e2aee1f4
Alex <alex@netness.co>
2020-12-06 22:50:12 +0100

    Basic interface for voteMove.

--- dist/components/procedure/vote/move.js
+++ dist/components/procedure/vote/move.js
@@ -1,17 +1,53 @@
+import { EMPTY_CID } from '@organigram/client-js/dist/ipfs';
 import React from 'react';
+import { useProcedure } from '../../../contexts/procedure';
 export const VoteMove = ({
   move
 }) => {
   const {
     procedure: {
-      data
+      data: vote
     }
   } = useProcedure();
-  return data ? /*#__PURE__*/React.createElement("div", {
+  if (!vote) return /*#__PURE__*/React.createElement(React.Fragment, null);
+  const proposition = vote.propositions && vote.propositions.find(p => p.moveKey && p.moveKey === move.key);
+  return /*#__PURE__*/React.createElement("div", {
     className: "procedure-move procedure-move-vote"
-  }, data.vote && /*#__PURE__*/React.createElement("button", {
+  }, /*#__PURE__*/React.createElement("pre", null, JSON.stringify(vote, null, 2)), !proposition ? /*#__PURE__*/React.createElement("button", {
+    className: "btn btn-primary",
+    onClick: () => vote.propose(move.key, EMPTY_CID, "0", // quorumSize
+    "0", // voteDuration
+    "0", // enactmentDuration
+    "0" // majoritySize
+    )
+  }, "Propose") : /*#__PURE__*/React.createElement(React.Fragment, null, /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("button", {
+    className: "btn btn-primary",
+    onClick: () => {
+      vote.vote(move.key, true).catch(error => console.error(error.message));
+    }
+  }, "Vote YES"), "}", /*#__PURE__*/React.createElement("button", {
+    className: "btn btn-primary",
+    onClick: () => {
+      vote.vote(move.key, false).catch(error => console.error(error.message));
+    }
+  }, "Vote NO")), /*#__PURE__*/React.createElement("button", {
     className: "btn btn-danger",
-    onClick: () => data.vote(move.key)
-  }, "Vote!")) : /*#__PURE__*/React.createElement(React.Fragment, null);
+    onClick: () => {
+      vote.veto(move.key, EMPTY_CID).catch(error => console.error(error.message));
+    }
+  }, "Veto"), /*#__PURE__*/React.createElement("button", {
+    className: "btn btn-primary",
+    onClick: () => {
+      vote.count(move.key).catch(error => {
+        console.error(error.message);
+        return false;
+      }).then(isYes => alert("Vote result is:", isYes ? "YES" : "NO"));
+    }
+  }, "Count"), /*#__PURE__*/React.createElement("button", {
+    className: "btn btn-success",
+    onClick: () => {
+      vote.enact(move.key).catch(error => console.error(error.message));
+    }
+  }, "Enact")));
 };
 export default VoteMove;
--- src/components/procedure/vote/move.js
+++ src/components/procedure/vote/move.js
@@ -1,16 +1,67 @@
+import { EMPTY_CID } from '@organigram/client-js/dist/ipfs'
 import React from 'react'
+import { useProcedure } from '../../../contexts/procedure'
 
 export const VoteMove = ({ move }) => {
-    const { procedure: { data } } = useProcedure()
-    return data ? (
+    const { procedure: { data: vote } } = useProcedure()
+    if (!vote) return <></>
+    const proposition = vote.propositions && vote.propositions.find(p => p.moveKey && p.moveKey === move.key)
+    return (
         <div className="procedure-move procedure-move-vote">
-            {data.vote && (
-                <button className="btn btn-danger" onClick={() => data.vote(move.key)}>
-                    Vote!
+            <pre>{JSON.stringify(vote, null, 2)}</pre>
+            {!proposition ? (
+                <button className="btn btn-primary" onClick={() => vote.propose(
+                    move.key,
+                    EMPTY_CID,
+                    "0",  // quorumSize
+                    "0",  // voteDuration
+                    "0",  // enactmentDuration
+                    "0"   // majoritySize
+                )}>
+                    Propose
                 </button>
+            ) : (
+                <>
+                    <div>
+                        <button className="btn btn-primary" onClick={() => {
+                            vote.vote(move.key, true)
+                            .catch(error => console.error(error.message))
+                        }}>
+                            Vote YES
+                        </button>}
+                        <button className="btn btn-primary" onClick={() => {
+                            vote.vote(move.key, false)
+                            .catch(error => console.error(error.message))
+                        }}>
+                            Vote NO
+                        </button>
+                    </div>
+                    <button className="btn btn-danger" onClick={() => {
+                        vote.veto(move.key, EMPTY_CID)
+                        .catch(error => console.error(error.message))
+                    }}>
+                        Veto
+                    </button>
+                    <button className="btn btn-primary" onClick={() => {
+                        vote.count(move.key)
+                        .catch(error => {
+                            console.error(error.message)
+                            return false
+                        })
+                        .then(isYes => alert("Vote result is:", isYes ? "YES" : "NO"))
+                    }}>
+                        Count
+                    </button>
+                    <button className="btn btn-success" onClick={() => {
+                        vote.enact(move.key)
+                        .catch(error => console.error(error.message))
+                    }}>
+                        Enact
+                    </button>
+                </>
             )}
         </div>
-    ) : <></>
+    )
 }
 
 export default VoteMove
